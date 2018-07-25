1dfef9ee150839d90952293d5b7d1ae5dc3fa6b8
Christina Kassab <ck617@ic.ac.uk>
2018-07-25 13:29:34 +0100

    Updated rolling ball - forces changed

--- mechanics-simulation/sketch.js
+++ mechanics-simulation/sketch.js
@@ -3,6 +3,7 @@ var xPos = 82;
 var angle = 0;
 var isRunning;
 var isPressed;
+var isResolvedPlane;
 var txt1, txt2, txt3, txt4, txt5;
 
 function setup() {
@@ -65,15 +66,26 @@ function setup() {
   var button6 = createButton("Resolve Forces in y and x");
   button6.position(650, 350);
   button6.mousePressed(function() {
-
   });
 
   var button7 = createButton("Resolve Forces in Plane");
   button7.position(950, 350);
   button7.mousePressed(function() {
-
+    isResolvedPlane = true;
   });
   angleMode(DEGREES);
+
+  txt3 = createDiv('Normal Force');
+  txt3.position(175, 150);
+  txt3.style('color', '#ff0000');
+
+  txt4 = createDiv('Weight');
+  txt4.position(80, 325);
+  txt4.style('color', '#00ff00');
+
+  txt5 = createDiv('Friction');
+  txt5.position(30, 150);
+  txt5.style('color', '#0000ff');
 
 }
 
@@ -94,26 +106,39 @@ function draw() {
 
   //Unresolved forces
   if (isPressed) {
+    //Reaction
     stroke(255, 0, 0)
-    line(150 + (sliderMass.value() / 5), 30 - (sliderMass.value() / 5), 70, 90); //Reaction
-    txt3 = createDiv('Normal Force');
-    txt3.position(175, 150);
-    txt3.style('font-size', '16px');
-    txt3.style('color', '#ff0000');
+    line(150 + (sliderMass.value() / 5), 30 - (sliderMass.value() / 5), 70, 90);
+    line(150 + (sliderMass.value() / 5), 30 - (sliderMass.value() / 5), 140, 25);
+    line(150 + (sliderMass.value() / 5), 30 - (sliderMass.value() / 5), 150, 40);
+    txt3.show();
 
+    //Weight
     stroke(0, 255, 0);
-    line(80, 80, 80, 150 + (sliderMass.value() / 5)); //Weight
-    txt4 = createDiv('Weight');
-    txt4.position(80, 325);
-    txt4.style('font-size', '16px');
-    txt4.style('color', '#00ff00');
+    line(80, 80, 80, 150 + (sliderMass.value() / 5));
+    line(70, 140, 80, 150 + (sliderMass.value()/5));
+    line(90, 140, 80, 150+(sliderMass.value()/5))
+    txt4.show();
 
+    //Friction
     stroke(0, 0, 255);
-    line(70, 90, 40 - (sliderFriction.value() / 5), 60 - (sliderFriction.value() / 5)); // Friction
-    txt5 = createDiv('Friction');
-    txt5.position(30, 150);
-    txt5.style('color', '#0000ff');
+    line(70, 90, 40 - (sliderFriction.value() / 5), 60 - (sliderFriction.value() / 5));
+    line(40, 80, 40 - (sliderFriction.value() / 5), 60 - (sliderFriction.value() / 5));
+    line(60, 60, 40 - (sliderFriction.value() / 5), 60 - (sliderFriction.value() / 5));
+    txt5.show();
+
+  /*  if(isResolvedPlane){
+      //Resolved weight
+      line(80, 80, 80*cos(45), (150 + (sliderMass.value() / 5))*cos(45));
+      line(80, 80, 80*sin(45), (150 + (sliderMass.value() / 5))*sin(45));
+
+    }*/
+  }else {
+    txt3.hide();
+    txt4.hide();
+    txt5.hide();
   }
+
 
 
   //Ball rolling
@@ -128,7 +153,7 @@ function draw() {
       ellipse(0, 0, 30, 30);
       fill(0, 110, 175);
       rect(0, 0, 5, 30);
-      angle++;
+      angle=angle+10;
       pop();
       xPos = xPos + 1;
       yPos = yPos + 1;
@@ -144,11 +169,15 @@ function draw() {
     if (xPos >= 300 || yPos >= 300) {
       xPos = 300;
       yPos = 300;
+
     }
   } else {
     //Ball sliding
     stroke(0);
     ellipse(xPos, yPos, 30, 30);
+    rectMode(CENTER);
+    fill(0, 110, 175);
+    rect(xPos, yPos, 5, 30);
     if (isRunning) {
       xPos = xPos + 1;
       yPos = yPos + 1;
